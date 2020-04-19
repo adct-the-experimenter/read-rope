452c313ab3266c91186c4b6a5692cf33169c2d56
Pablo Camacho <bringerofawesomefood@gmail.com>
2020-04-18 18:50:00 -0700

    Changed arduino firmware. Added more to library.

---
+++ read_rope_shared_library/test/CMakeFiles/readrope.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/readrope.dir/src/readrope.cpp.o"
+  "libreadrope.pdb"
+  "libreadrope.so"
+  "libreadrope.so.0"
+  "libreadrope.so.0.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/readrope.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
